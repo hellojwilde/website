b56f14478a74ea190a382d3157c2c6d563a5c24d
Jonathan Wilde <hello@jwilde.me>
2022-06-18 09:10:57 -0700

    Posthog

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,7 @@
 import "./src/styles/Global.css";
+import "posthog-js";
+
+posthog.init("phc_FiE4M3UP7J2MU9sACzX46IZBLXNPVEfGQBK7g5Pitzb", {
+  api_host: "https://app.posthog.com",
+  persistence: "memory",
+});
