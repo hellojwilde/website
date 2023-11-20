bbab6ea650ef11e3352d716cf94a3f9d74e0d1a7
Jonathan Wilde <jonathan@realworldprivacy.com>
2023-11-19 17:22:34 -0700

    cleanup

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ function Footer(props) {
     <footer className="container mx-auto px-6 mt-8 md:mt-12">
       <div className="border-t-2 text-sm text-slate-500 py-8 md:py-12 md:flex md:justify-between">
         <p className="text-center md:text-left">
-          &copy; 2008 - {new Date().getFullYear()} Real World Privacy LLC
+          &copy; 2008 - {new Date().getFullYear()} Jonathan Wilde
         </p>
         <ul className="flex space-x-4 justify-center md:justify-end">
           <li>
